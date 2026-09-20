4b8f3a148303c8cccb6600fcaacb1a769a1c9459
Harika-Kovvuri <harika21613@gmail.com>
2026-09-20 11:56:44 +0530

    Time: 30 ms (25.69%), Space: 51.2 MB (35.23%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -1,30 +1,29 @@
 class Solution {
-    public int maxim(int[] nums) {
-        int maxi = Integer.MIN_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            maxi = Math.max(maxi, nums[i]);
+    public int findMax(int[] nums){
+        int maxi=nums[0];
+        for(int num:nums){
+            maxi=Math.max(maxi,num);
         }
         return maxi;
     }
 
-    int dsum(int[] nums, int div) {
-        int n = nums.length;
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += Math.ceil((double) nums[i] / (double) div);
+    public int sum(int[] nums, int divisor){
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            ans+=Math.ceil((double)nums[i]/(double)divisor);
         }
-        return sum;
+        return ans;
     }
-
     public int smallestDivisor(int[] nums, int threshold) {
-        int low = 1;
-        int high = maxim(nums);
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (dsum(nums, mid) <= threshold) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        int low=1;
+        int high=findMax(nums);
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(sum(nums,mid)<=threshold){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
         return low;
