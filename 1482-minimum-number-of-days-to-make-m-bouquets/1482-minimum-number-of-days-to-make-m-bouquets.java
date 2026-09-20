584db9b49c99e28c354733ba02dd1cfd5e11c4e3
Harika-Kovvuri <harika21613@gmail.com>
2026-09-20 11:21:36 +0530

    Time: 19 ms (37.55%), Space: 82.8 MB (28.63%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,52 +1,54 @@
 class Solution {
-    public int findmax(int[] bloomday) {
-        int maxi = Integer.MIN_VALUE;
-        for (int i = 0; i < bloomday.length; i++) {
-            maxi = Math.max(maxi, bloomday[i]);
-        }
-        return maxi;
-    }
-
-    public int findmini(int[] bloomday) {
-        int mini = Integer.MAX_VALUE;
-        for (int i = 0; i < bloomday.length; i++) {
-            mini = Math.min(mini, bloomday[i]);
-        }
-        return mini;
-    }
-
-    public boolean possible(int[] bloomDay, int day, int m, int k) {
-        int noOfBouqets = 0;
-        int count = 0;
+    public boolean possible(int[] bloomDay, int day, int bouquets, int flowers) {
+        int cnt = 0;
+        int noOfBouquets = 0;
         for (int i = 0; i < bloomDay.length; i++) {
             if (bloomDay[i] <= day) {
-                count++;
+                cnt++;
             } else {
-                noOfBouqets += (count / k);
-                count = 0;
+                noOfBouquets += (cnt / flowers);
+                cnt = 0;
             }
         }
-        noOfBouqets += (count / k);
-        if (noOfBouqets >= m) {
+        noOfBouquets += (cnt / flowers);
+
+        if (noOfBouquets >= bouquets) {
             return true;
         }
         return false;
     }
 
+    public int findMaxi(int[] bloomDay) {
+        int maxi = bloomDay[0];
+        for (int num : bloomDay) {
+            maxi = Math.max(num, maxi);
+        }
+        return maxi;
+    }
+
+    public int findMini(int[] bloomDay) {
+        int mini = bloomDay[0];
+        for (int num : bloomDay) {
+            mini = Math.min(num, mini);
+        }
+        return mini;
+    }
+
     public int minDays(int[] bloomDay, int m, int k) {
-        if ((long) m * (long) k > (long) bloomDay.length)
+        if (m * k > bloomDay.length)
             return -1;
-        int high = findmax(bloomDay);
-        int low = findmini(bloomDay);
-        while (low <= high) {
-            int mid = (low + high) / 2;
+        int low=findMini(bloomDay);
+        int high=findMaxi(bloomDay);
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
             if (possible(bloomDay, mid, m, k)) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
-        return low;
-
+        return ans;
     }
 }
